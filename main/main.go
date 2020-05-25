2fe5f5031a15c9ddf95d71e178175040440057ee
Fenrir <zhanglin0821@gmail.com>
2020-05-24 22:35:11 -0700

    concurrent run all crawlers

--- main/main.go
+++ main/main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"dmhyCrawler/conf"
 	"dmhyCrawler/crawler"
+	"fmt"
+	"sync"
+	"time"
 )
 
-func main() {
-	serviceConf := conf.GetServiceConfig()
-	for _, bangumiConfig := range serviceConf.BangumiConfigs {
-		url := "https://share.dmhy.org/topics/list?keyword=" + bangumiConfig.SearchExp
-		myCrawler := crawler.NewCrawler(bangumiConfig.Title, url)
-		myCrawler.FetchMagLinks()
+var wg sync.WaitGroup
 
+func main() {
+	start := time.Now()
+
+	serviceConf := conf.GetServiceConfig()
+
+	for _, bangumiConfig := range serviceConf.BangumiConfigs {
+		if bangumiConfig.SearchExp != "" && bangumiConfig.Title != "" {
+			wg.Add(1)
+			go func(title string, searchExp string) {
+				url := "https://share.dmhy.org/topics/list?keyword=" + searchExp
+				myCrawler := crawler.NewCrawler(title, url)
+				myCrawler.FetchMagLinks()
+				defer wg.Done()
+			}(bangumiConfig.Title, bangumiConfig.SearchExp)
+		}
 	}
+	wg.Wait()
+	timeUsed := time.Now().Sub(start)
+	fmt.Printf("job finished, time used %v\n", timeUsed)
 }
